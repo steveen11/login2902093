3630046393453d880707cc4179fcf836764b4e50
steveen11 <asamuelstevrn@gmail.com>
2024-07-20 13:25:54 -0500

    feat: se crea PostOfficeApp

--- app/src/main/java/co/edu/sena/login2902093/MainActivity.kt
+++ app/src/main/java/co/edu/sena/login2902093/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import co.edu.sena.login2902093.app.PostOfficeApp
 
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            PostOfficeApp()
                 }
             }
 }
---
+++ app/src/main/java/co/edu/sena/login2902093/app/PostOfficeApp.kt
@@ -0,0 +1,18 @@
+package co.edu.sena.login2902093.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PostOfficeApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ){
+
+
+    }
+}
